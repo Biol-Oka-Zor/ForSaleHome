73437d52c9a94d80b8fb1e12a48ab9c6989ac7f8
user <biloltohirhonov@gmail.com>
2025-11-18 09:36:19 +0500

    add project

---
+++ src/main/java/org/example/MyBotService.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public class MyBotService {
+
+    public SendMessage sendMessage(){
+
+        SendMessage sendMessage = new SendMessage();
+
+        return  sendMessage;
+    }
+
+}
